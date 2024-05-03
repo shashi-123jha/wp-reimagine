f70b9c36132e7df539d3d955ae1901ce1f7d6ceb
shashi-123jha <ranjanshashi854105@gmail.com>
2024-05-03 21:58:10 +0530

    fixed nav bar position

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // locomotive.js code 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
+    el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     lerp:0.05,
 });
