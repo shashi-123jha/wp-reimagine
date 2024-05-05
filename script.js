d91e8a72545cba62da4069020f6cfcbd2871226e
shashi-123jha <ranjanshashi854105@gmail.com>
2024-05-06 00:40:47 +0530

    2nd page animation

--- script.js
+++ script.js
@@ -5,5 +5,51 @@ const scroll = new LocomotiveScroll({
     lerp:0.1,
 });
 
+var crsr = document.querySelector("#cursor")
+var crsrb = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove" , function(dets){
+crsr.style.left = dets.x+30+"px",
+crsr.style.top = dets.y+"px"
+
+// for the blur parts 
+crsrb.style.left = dets.x - 250 +"px",
+crsrb.style.top = dets.y - 250 +"px"
+   
+})
 
 
+
+document.addEventListener("mousemove" , function(dets){
+
+   
+})
+
+
+// on hover image will  be shown on  the page 
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+  });
